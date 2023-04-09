fbc3731cd37a8f444e6b7c17a667c0b741493829
Lina_Held_Void <iltep118666@gmail.com>
2023-04-09 21:45:24 +0400

    Бот умеет спрашивать про пол и город партнёра

--- bot_functions.py
+++ bot_functions.py
@@ -1,87 +1,80 @@
-from heapq import nlargest
 import configparser
-from pprint import pprint
-import requests
-import json
+from random import randrange
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+import common_functions
 
 
-class VK:
+config = configparser.ConfigParser()
+config.read("TOKENS_DANGER.ini")
+token = config['VK']['bot_token']
 
-    def __init__(self, version='5.131'):
-        config = configparser.ConfigParser()
-        config.read("token_list.ini")
-        self.token = config['VK']['access_token']
-        self.id = config['VK']['user_id']
-        self.version = version
-        self.params = {'access_token': self.token, 'v': self.version}
-        self.data_point = self.id.isdigit()
-
-    def users_info(self):                                                       # получение информации о пользователе
-        url = 'https://api.vk.com/method/users.get'
-        params = {'user_ids': self.id, 'fields': 'domain'}
-        response = requests.get(url, params={**self.params, **params})
-
-        return response.json()
-
-    def get_photos_data(self, owner_id, token, offset=0):          # получение информации о фото из ВК
-        self.id = owner_id
-        self.token = token
-        url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': self.id,
-                  'album_id': 'profile',
-                  'access_token': self.token,
-                  'rev': 0,
-                  'extended': 1,
-                  'v': self.version,
-                  'photo_sizes': 1,
-                  'offset': offset,
-                  }
-        response = requests.get(url, params=params)
-        link = f'https://vk.com/{self.id}'
-        print(link)
-        return json.loads(response.text)
-
-    def data_for_db(self):
-        data = (self.users_info())
-        pprint(data)
-        name = data['response'][0]['first_name']
-        second_name = data['response'][0]['last_name']
-        link = data['response'][0]['domain']
-        super_link = f'https://vk.com/{link}'
-        data = self.get_photos_data(self.id, self.token)
-        count_foto = data['response']['count']
-        i = 0
-        likes_counter = []                                          # находим самые популярные фото (по лайкам)
-        while i < count_foto:
-            likes = data['response']['items'][i]['likes']['count']
-            likes_counter.append(likes)
-            i += 1
-        likes_leaders = nlargest(3, likes_counter)
-        print(likes_leaders)
-
-        j = 0
-        new_data = []
-        while j < count_foto:
-            if data['response']['items'][j]['likes']['count'] in likes_leaders:
-                new_data.append(data['response']['items'][j])
-            j += 1
-
-        link_list = []
-        k = 0
-        while k < len(likes_leaders):                                     # отбираем версии фото лучшего качества
-            max_height = 0
-            for pics in new_data[k]['sizes']:
-                if max_height < pics['height']:
-                    max_height = pics['height']
-            print(max_height)
-            for pic in new_data[k]['sizes']:
-                print(pic)
-                if pic['height'] == max_height:
-                    print(max_height)
-                    link_list.append(pic['url'])
-            k += 1
-        return link_list, name, second_name, super_link
+session = vk_api.VkApi(token=token)
 
 
-vk = VK()
-pprint(vk.data_for_db())
+def write_msg(user_id, message, keyboard=None):
+    params = {
+        'user_id': user_id,
+        'message': message,
+        'random_id': randrange(10 ** 7),
+    }
+    if keyboard != None:
+        params['keyboard'] = keyboard.get_keyboard()
+    else:
+        params = params
+
+    session.method('messages.send', params)
+
+
+def location(user_id):
+    write_msg(user_id, 'Отлично! Теперь укажите ваш город.'
+                       'Если локация для вас не важна, напишите "не важно"')
+    for event in VkLongPoll(session).listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            text = event.text
+            if text == 'не важно' or text == 'Не важно':
+                city = 0
+            else:
+                city = text
+            return city
+
+
+for event in VkLongPoll(session).listen():
+
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        text = event.text
+        user_id = event.user_id
+
+        if text == 'привет' or text == 'Привет':
+            keyboard_1 = VkKeyboard()
+            keyboard_1.add_button('Вперёд!', VkKeyboardColor.POSITIVE)
+
+            write_msg(user_id, 'Вы готовы встретиться со своей судьбой?)', keyboard_1)
+
+        elif text == 'Вперёд!':
+            keyboard_2 = VkKeyboard()
+            keyboard_2.add_button('Парня', VkKeyboardColor.PRIMARY)
+            keyboard_2.add_line()
+            keyboard_2.add_button('Девушку', VkKeyboardColor.PRIMARY)
+            keyboard_2.add_line()
+            keyboard_2.add_button('Пол не важен', VkKeyboardColor.PRIMARY)
+
+            write_msg(user_id, 'Кого вы ищете?', keyboard_2)
+
+        elif text == 'Парня':
+            gender = '2'
+            location = location(user_id)
+        elif text == 'Девушку':
+            gender = '1'
+            location = location(user_id)
+        elif text == 'Пол не важен':
+            gender = '0'
+            location = location(user_id)
+
+
+        else:
+            write_msg(user_id, 'Извините, ваш ответ не распознан. Напишите боту "привет" для начала поиска')
